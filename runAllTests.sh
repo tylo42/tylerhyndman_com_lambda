ed6f6347187858fd9b5da36136611a3365a8c856
Tyler Hyndman <tyler.hyndman@gmail.com>
2021-03-29 15:35:05 -0500

    Move some commands from runAllTests.sh to package.json

--- runAllTests.sh
+++ runAllTests.sh
@@ -6,22 +6,24 @@ function cleanup()
     if [ ! -z ${PID_OF_LAMBDA+x} ]; then
         kill -SIGINT $PID_OF_LAMBDA
     fi
-    docker-compose down
+    npm run docker:stop
 }
 
 trap cleanup EXIT
 
 npm run check
-npm run test
+npm run test:unit
 
-docker-compose up -d
+npm run docker:run
 
 npx lambda-local -l src/index.js -h handler --watch 8081 &
 PID_OF_LAMBDA=$!
 sleep 1
 
-aws --endpoint-url http://localhost:9324 sqs send-message --queue-url http://localhost:9324/queue/default --message-body "Testing" | cat
+echo "HERE: $PID_OF_LAMBDA"
+ps aux | grep lambda-local
 
+aws --endpoint-url http://localhost:9324 sqs send-message --queue-url http://localhost:9324/queue/default --message-body "Testing" | cat
 kill -0 $PID_OF_LAMBDA # Will exit if the lambda process is not running
 
-npm run e2e
+npm run test:e2e
